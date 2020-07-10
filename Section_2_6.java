50e1f90d9d3be50959b7a31a5c98c1b48738c717
adellaiz <68011080+adellaiz@users.noreply.github.com>
2020-07-10 11:37:40 +0700

    Add files via upload

---
+++ Section_2_6.java
@@ -0,0 +1,21 @@
+/**
+ * @author [Della A. Izzah]
+ * @email [adellaiz@student.ub.ac.id]
+ * @create date 2020-07-10 11:33:36
+ * @modify date 2020-07-10 11:37:14
+ * @desc [description]
+ */
+package Chapter_2_Section;
+import java.util.Scanner;
+public class Section_2_6 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number between 0 and 1000 = ");
+        int digits = input.nextInt();
+        int a = digits%10;// 932 % 10 = 2
+        int b = digits/10;// 932 / 10 = 93
+        int c = b/10;// 93 / 10 = 9
+        int d = b%10;// 93 % 10 = 3;
+        System.out.println("The sum of the digits is = " + a + "+" + c + "+" + d + " = " + (a+c+d));
+    }
+}
